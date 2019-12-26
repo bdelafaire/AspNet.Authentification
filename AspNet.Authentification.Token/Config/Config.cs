f8516832e952979a54968581a7b59e56fa68b690
batdelaf <delafaire@genapi.fr>
2019-12-26 22:33:23 +0100

    add token authy

---
+++ AspNet.Authentification.Token/Config/Config.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet.Authentification.Token.Config
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","MY API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId="client",
+
+                    //definition du client
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    
+                    // contexte que le client peut accéder
+                    AllowedScopes = {"api1"}
+
+                }
+            };
+        }
+    }
+}
